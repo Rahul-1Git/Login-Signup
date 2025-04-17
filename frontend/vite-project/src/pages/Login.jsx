5c9cb929ad9f5d347d36443a77184cae51c334e2
Rahul-1Git <99199444+Rahul-1Git@users.noreply.github.com>
2025-04-18 01:16:06 +0530

    Update Login.jsx

--- frontend/vite-project/src/pages/Login.jsx
+++ frontend/vite-project/src/pages/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
       return handleError("name, email and password are required");
     }
     try {
-      const url = "http://localhost:5000/auth/login";
+      const url = "https://login-signup-back.vercel.app/login";
       const response = await fetch(url, {
         method: "POST",
         headers: {
